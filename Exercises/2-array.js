49482a475303972925146ed171769f6ea98a47c2
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2024-12-05 07:58:03 +0200

    - lab work done

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  function obj(i) {
+    return arr[i];
+  }
+  obj.push = (val) => arr.push(val);
+  obj.pop = () => arr.pop();
+  return obj;
+};
 
 module.exports = { array };
